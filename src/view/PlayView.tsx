7bb8496ae3be8563c9fe3f757f86fa93b2d73d0f
Rusty Bentley <mycoding.alert@gmail.com>
2021-05-10 16:04:34 -0500

    converts PlayView to class component

--- src/view/PlayView.tsx
+++ src/view/PlayView.tsx
@@ -1,3 +1,13 @@
-export const PlayView = () => {
-  return <div> Play View </div>;
+import { Component } from 'react';
+
+interface GameState {
+
+}
+
+export class PlayView extends Component<any, GameState> {
+  public render() {
+    return (
+      <div>Play View</div>
+    )
+  }
 };
