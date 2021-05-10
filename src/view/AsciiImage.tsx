05632e2d6c313a22273e8ae9c6c158a75cbd8aaf
Rusty Bentley <mycoding.alert@gmail.com>
2021-05-10 16:00:22 -0500

    more prettier formatting

--- src/view/AsciiImage.tsx
+++ src/view/AsciiImage.tsx
@@ -1,24 +1,26 @@
-import classNames from 'classnames';
+import classNames from "classnames";
 
 interface AsciiImageProps {
-    imageSrc: string;
-    alt: string;
-    className?: string;
+  imageSrc: string;
+  alt: string;
+  className?: string;
 }
 
 const AsciiImage = (props: AsciiImageProps) => {
-    const lines = props.imageSrc.split('\n').map((line, index) => (
-        <div className={classNames('imageLine', props.className)} key={index}>
-            {line}
-        </div>
-    ));
-    return (
-        <div className={classNames('image', props.className)} 
-             role="figure" 
-             aria-label={props.alt}>
-            {lines}
-        </div>
-    );
-}
+  const lines = props.imageSrc.split("\n").map((line, index) => (
+    <div className={classNames("imageLine", props.className)} key={index}>
+      {line}
+    </div>
+  ));
+  return (
+    <div
+      className={classNames("image", props.className)}
+      role="figure"
+      aria-label={props.alt}
+    >
+      {lines}
+    </div>
+  );
+};
 
 export default AsciiImage;
