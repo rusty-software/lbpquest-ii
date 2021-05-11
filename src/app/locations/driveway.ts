ad68ccb3c94cd6e1f23d6a21916a4e897796a925
Rusty Bentley <mycoding.alert@gmail.com>
2021-05-11 09:34:17 -0500

    fixing the map arrow

--- src/app/locations/driveway.ts
+++ src/app/locations/driveway.ts
@@ -14,9 +14,7 @@ export class Driveway implements Location {
   description(): string {
     let s =
       "You are in the driveway. The heat boils off of the pavement in shimmering waves as the sun beats down on you from an unmerciful summer sky.";
-    this.items.map((item) => {
-      s += `\n\nThere is a ${item.name} here.`;
-    })
+    this.items.map((item) => (s += `\n\nThere is a ${item.name} here.`));
     return s;
   }
 }
