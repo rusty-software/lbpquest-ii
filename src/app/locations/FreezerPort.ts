884380537516c498d891df0242b0e2ed2662dd09
Rusty Bentley <mycoding.alert@gmail.com>
2021-05-16 19:02:44 -0500

    forgot to save a file

--- src/app/locations/FreezerPort.ts
+++ src/app/locations/FreezerPort.ts
@@ -1,7 +1,11 @@
 import { BaseLocation } from "./BaseLocation";
 
 export class FreezerPort extends BaseLocation {
+  public readonly title = "Freezer Port";
+  descriptionText =
+    "You are in what appears to be a small car port with a walk-in freezer in the back. The freezer door looks unlocked.";
+
   enter(): void {
-    throw new Error("Method not implemented.");
+    this.entered = true;
   }
 }
