fdba8b0e4d2ed5cb89f9e52ceb2c30a7287a1074
Rusty Bentley <mycoding.alert@gmail.com>
2021-05-21 13:11:54 -0500

    adds construction paper and crayon details

--- src/app/items/ConstructionPaper.ts
+++ src/app/items/ConstructionPaper.ts
@@ -1,3 +1,25 @@
 import { BaseItem } from "./BaseItem";
+import { ItemKey } from "./ItemKey";
+import { GameEngine } from "../GameEngine";
 
-export class ConstructionPaper extends BaseItem {}
+export class ConstructionPaper extends BaseItem {
+  public id = ItemKey.ConstructionPaper;
+  public name = "construction paper";
+  public value = 5;
+
+  public canTake(gameEngine: GameEngine): boolean {
+    return true;
+  }
+  public take(gameEngine: GameEngine): string {
+    return "You roll up the construction paper and put it neatly into your duffle bag.";
+  }
+  public drop(gameEngine: GameEngine): string {
+    return "You drop the construction paper on the ground. It unrolls like a scroll, revealing a blank page.";
+  }
+  examine(gameEngine: GameEngine): string {
+    return "The construction paper is sturdy and roughly textured. Everything that plain ol' construction paper should be...";
+  }
+  use(gameEngine: GameEngine): string {
+    return "TODO: can be used in combination with other arts and crafts supplies";
+  }
+}
--- src/app/items/Crayons.ts
+++ src/app/items/Crayons.ts
@@ -1,3 +1,25 @@
 import { BaseItem } from "./BaseItem";
+import { ItemKey } from "./ItemKey";
+import { GameEngine } from "../GameEngine";
 
-export class Crayons extends BaseItem {}
+export class Crayons extends BaseItem {
+  public id = ItemKey.Crayons;
+  public name = "crayons";
+  public value = 5;
+
+  public canTake(gameEngine: GameEngine): boolean {
+    return true;
+  }
+  public take(gameEngine: GameEngine): string {
+    return "You place the crayons into your duffle bag. They don't seem to be in danger of melting, as they're at least partially in the refrigerated part of the bag.";
+  }
+  public drop(gameEngine: GameEngine): string {
+    return "You drop the crayons. You're not sure how long they'll last given the current climate.";
+  }
+  examine(gameEngine: GameEngine): string {
+    return "While the collection of crayons is not prolific, they all appear to be in good repair.";
+  }
+  use(gameEngine: GameEngine): string {
+    return "TODO: can be used in combination with other arts and crafts supplies";
+  }
+}
